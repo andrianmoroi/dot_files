fd9c0692c2cfc871a09b64e5708dcfb9e374bbb2
andrian <andrian.moroi@gmail.com>
2024-01-05 18:50:44 +0200

    Updated prompt.

--- bash_config/scripts/prompt.sh
+++ bash_config/scripts/prompt.sh
@@ -2,4 +2,4 @@ parse_git_branch() {
    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
 }
 
-PS1=$'\\e[1;34m\\W\\e[1;33m$(parse_git_branch)\\e[40m\\e[0;37m '
+PS1=$'\\e[1;34m\\W\\e[1;33m$(parse_git_branch)\\e[40m\\e[0;37m ⮞ '
