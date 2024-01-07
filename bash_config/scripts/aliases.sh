7fa7ffae15c295a7a2721a1e473aa4bb3dea62de
andrian <andrian.moroi@gmail.com>
2024-01-07 15:05:21 +0200

    Fixed sync config.

--- bash_config/scripts/aliases.sh
+++ bash_config/scripts/aliases.sh
@@ -2,4 +2,4 @@ alias n="nvim"
 alias gs="git status"
 alias gc="git add . && git commit -m"
 
-alias sync-configs="cd ~/.dot_files && git pull origin main && ./install.sh && cd - >> /dev/null"
+alias sync-configs="cd ~/.dot_files && git pull origin main && bash_config/install.sh && cd - >> /dev/null"
