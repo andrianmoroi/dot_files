16179940c882927163705c23ed47021fae8ee591
andrian <andrian.moroi@gmail.com>
2024-01-07 16:12:21 +0200

    Updated aliases.

--- bash_config/scripts/aliases.sh
+++ bash_config/scripts/aliases.sh
@@ -1,5 +1,6 @@
 alias n="nvim"
 alias gs="git status"
 alias gc="git add . && git commit -m"
+alias gl="git log --oneline"
 
 alias sync-configs="cd ~/.dot_files/bash_config && git pull origin main && ./install.sh && cd - >> /dev/null"
