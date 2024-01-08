610c0d22188f28f3e625f034a2288d386cff19cb
andrian <andrian.moroi@gmail.com>
2024-01-08 23:17:36 +0200

    Added exit code to the prompt.

--- bash_config/scripts/prompt.sh
+++ bash_config/scripts/prompt.sh
@@ -1,6 +1,14 @@
-parse_git_branch() {
+function _parse_git_branch() {
    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
 }
 
-PS1=$'\\e[1;34m\\W\\e[1;33m$(parse_git_branch)\\e[40m\\e[0;37m ⮞ '
-PS1='\[\033[1;34m\]\W\[\033[0m\]\[\033[1;33m\]$(parse_git_branch)\[\033[0m\] \[\033[1;36m\]$\[\033[0m\] '
+# ⛔ -- 26d4
+# ❌ -- 274c
+function _exit_code() {
+    if [ $? != 0 ]; then
+        echo "❌ "
+    fi
+}
+
+PS1=$'\\e[1;34m\\W\\e[1;33m$(_parse_git_branch)\\e[40m\\e[0;37m ⮞ '
+PS1='$(_exit_code)\[\033[1;34m\]\W\[\033[0m\]\[\033[1;33m\]$(_parse_git_branch)\[\033[0m\] \[\033[1;36m\]$\[\033[0m\] '
