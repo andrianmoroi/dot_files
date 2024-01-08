6d737344fb68b196efda364e032851a0a9db26a7
andrian <andrian.moroi@gmail.com>
2024-01-09 00:04:10 +0200

    Added bookmarks command.

---
+++ bash_config/scripts/bookmarks.sh
@@ -0,0 +1,26 @@
+BOOKMARKS_FILE=~/.config/bookmarks.txt
+
+function _g_path() {
+    echo $(grep "^$1\s" $BOOKMARKS_FILE | awk -F ' ' '{print $2}')
+}
+
+function _g_options() {
+
+    if [ -f "$BOOKMARKS_FILE" ] && [ "${#COMP_WORDS[@]}" == "2" ]; then
+        local all_options=$(awk -F ' ' '{print $1}' $BOOKMARKS_FILE)
+        local show_options=$(compgen -W "$all_options" -- "${COMP_WORDS[1]}")
+
+        COMPREPLY=($show_options)
+    else
+        return
+    fi
+}
+
+# Go function to a folder stored in bookmark
+function g() {
+    local path=`_g_path $1`
+
+    cd $path
+}
+
+complete -F _g_options g
