f09e9f78402b41a5c5a8b8e674a7e26c6978d03d
Andrian Moroi <andrian.moroi@qiagen.com>
2024-01-08 22:36:00 +0200

    Trying new prompt command line.

--- bash_config/scripts/prompt.sh
+++ bash_config/scripts/prompt.sh
@@ -3,3 +3,4 @@ parse_git_branch() {
 }
 
 PS1=$'\\e[1;34m\\W\\e[1;33m$(parse_git_branch)\\e[40m\\e[0;37m ⮞ '
+PS1='\[\033[1;34m\]\W\[\033[0m\]\[\033[1;33m\]$(parse_git_branch)\[\033[0m\] \[\033[1;36m\]$\[\033[0m\] '
