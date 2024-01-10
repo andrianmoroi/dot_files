31e433ff1ad750cff9e5b5ee900a5b815c220f4a
andrian <andrian.moroi@gmail.com>
2024-01-10 21:15:18 +0200

    Updated nvim keymaps.

--- nvim_config/lua/keymaps.lua
+++ nvim_config/lua/keymaps.lua
@@ -1,4 +1,5 @@
 local opts = { noremap = true, silent = true }
+local builtin = require("telescope.builtin")
 
 -- set custom buffers commands
 vim.api.nvim_set_keymap("n", "<leader>w", ":w<cr>", opts)
@@ -11,3 +12,7 @@ vim.api.nvim_set_keymap("n", "<leader>e", ":Lexplore 20<cr>", opts)
 -- open telescope
 vim.api.nvim_set_keymap("n", "<leader>b", ":Telescope buffers<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<cr>", opts)
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope git_files<cr>", opts)
+vim.keymap.set("n", "<C-f>", function ()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end, opts)
