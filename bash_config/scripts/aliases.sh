f077107d14df6b986a10942fa8d60f132d75dd3a
Andrian Moroi <andrian.moroi@gmail.com>
2024-10-06 00:54:57 +0300

    Add l alias (list the content of the folder).

--- bash_config/scripts/aliases.sh
+++ bash_config/scripts/aliases.sh
@@ -3,4 +3,7 @@ alias gs="git status"
 alias gc="git add . && git commit -m"
 alias gl="git log --oneline"
 
+alias l='lsd --group-dirs first -lAhF'
+
+
 alias sync-configs="cd ~/.config/.dot_files/bash_config && git pull origin main && ./install.sh && cd - >> /dev/null"
