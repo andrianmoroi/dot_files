df4c149b5f19f5cd72e3e248f71ce0d3ef13e3f9
Andrian Moroi <andrian.moroi@qiagen.com>
2024-10-11 10:47:22 +0300

    Use ls instead of lsd in aliases.

--- bash_config/scripts/aliases.sh
+++ bash_config/scripts/aliases.sh
@@ -3,7 +3,8 @@ alias gs="git status"
 alias gc="git add . && git commit -m"
 alias gl="git log --oneline"
 
-alias l='lsd --group-dirs first -lAhF'
-
+# alias l='lsd --group-dirs first -lAhF'
+alias l='ls --color --group-directories-first -lAhF'
+alias b="cat ~/.config/bookmarks.txt | awk '{print \$1}' | grep .| fzf --preview \"cat ~/.config/bookmarks.txt | grep '^{}' | sed 's|^[^ \t]*\s*||' | xargs tree -L 1 | head -n 20\""
 
 alias sync-configs="cd ~/.config/.dot_files/bash_config && git pull origin main && ./install.sh && cd - >> /dev/null"
