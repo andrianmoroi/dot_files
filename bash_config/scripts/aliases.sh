04ac4a4d14161575ebdd7f1fc13f2acaaccc1379
Andrian Moroi <andrian.moroi@gmail.com>
2024-10-16 23:06:22 +0300

    Add tmx alias.

--- bash_config/scripts/aliases.sh
+++ bash_config/scripts/aliases.sh
@@ -7,4 +7,6 @@ alias gl="git log --oneline"
 alias l='ls --color --group-directories-first -lAhF'
 alias b="cat ~/.config/bookmarks.txt | awk '{print \$1}' | grep .| fzf --preview \"cat ~/.config/bookmarks.txt | grep '^{}' | sed 's|^[^ \t]*\s*||' | xargs tree -L 1 | head -n 20\""
 
+alias tmx="tmux new-session -s \$(pwd | sed 's|.*/||')"
+
 alias sync-configs="cd ~/.config/.dot_files/bash_config && git pull origin main && ./install.sh && cd - >> /dev/null"
