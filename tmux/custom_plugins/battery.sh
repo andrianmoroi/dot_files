863eb023e303de3f5fcbf6493a88574268bdab44
Andrian Moroi <andrian.moroi@qiagen.com>
2024-10-25 18:59:00 +0300

    Create custom plugin for battery status.

---
+++ tmux/custom_plugins/battery.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+status=$(cat /sys/class/power_supply/BAT1/status)
+
+normal_icons=("󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
+charge_icons=("󰢟" "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅")
+
+icons=("${normal_icons[@]}")
+
+case $status in
+    discharging|Discharging)
+        icons=("${normal_icons[@]}")
+      ;;
+    high|Full)
+        icons=("${charge_icons[@]}")
+      ;;
+    charging|Charging)
+        icons=("${charge_icons[@]}")
+      ;;
+    *)
+        icons=("${normal_icons[@]}")
+      ;;
+esac
+
+index=$((capacity/10))
+
+echo ${icons[index]}
