e610bc09b2d7a1fe78319f541edd77aaa91475a8
Andrian Moroi <andrian.moroi@qiagen.com>
2024-10-28 21:02:27 +0200

    Update status line.

--- nvim_config/lua/plugins/echasnovski-mini.lua
+++ nvim_config/lua/plugins/echasnovski-mini.lua
@@ -1,28 +1,34 @@
--- TODO: Investigate if needs this plugin
-return { -- Collection of various small independent plugins/modules
+return {
     "echasnovski/mini.nvim",
     config = function()
-        -- -- Better Around/Inside textobjects
-        -- --
-        -- -- Examples:
-        -- --  - va)  - [V]isually select [A]round [)]paren
-        -- --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-        -- --  - ci'  - [C]hange [I]nside [']quote
-        -- require("mini.ai").setup({ n_lines = 500 })
-        --
-        -- -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        -- --
-        -- -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- -- - sd'   - [S]urround [D]elete [']quotes
-        -- -- - sr)'  - [S]urround [R]eplace [)] [']
-        -- require("mini.surround").setup()
-
-        -- Simple and easy statusline.
-        --  You could remove this setup call if you don't like it,
-        --  and try some other statusline plugin
         local statusline = require("mini.statusline")
-        -- set use_icons to true if you have a Nerd Font
-        statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+        statusline.setup({
+            use_icons = vim.g.have_nerd_font,
+            set_vim_settings = true,
+            content = {
+                active = function()
+                    local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 1000000 })
+                    local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 1000000 })
+                    local location      = MiniStatusline.section_location({ trunc_width = 75 })
+                    local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+                    return MiniStatusline.combine_groups({
+                        { hl = mode_hl,                  strings = { mode } },
+                        '%<', -- Mark general truncate point
+                        { hl = 'MiniStatuslineFilename', strings = { "%{expand('%:~:.')}" } },
+                        '%=', -- End left alignment
+                        { hl = 'MiniStatuslineDevinfo',  strings = { vim.fn.reg_recording() ~= "" and "Recording: " .. vim.fn.reg_recording() or "" } },
+                        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                        '%<', -- Mark general truncate point
+                        { hl = mode_hl, strings = { search, location } },
+                    })
+                end,
+                inactive = function()
+                    return "%{expand('%:~:.')}"
+                end
+            }
+        })
 
         -- You can configure sections in the statusline by overriding their
         -- default behavior. For example, here we set the section for
