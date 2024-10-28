acc9c69753ed958872ae7e66d7525f0a8d558d94
Andrian Moroi <andrian.moroi@qiagen.com>
2024-10-28 20:09:23 +0200

    Add colors variables to bashrc file.

--- bash_config/install.sh
+++ bash_config/install.sh
@@ -3,10 +3,10 @@ echo "Updating '.bashrc' file with latest configuration files."
 
 for f in ./scripts/*
 do
-	full_path=$(realpath "$f")
+    full_path=$(realpath "$f")
 
-	if ! grep "source $full_path" ~/.bashrc > /dev/null; then
-		echo "Adding $f source file to the bashrc."
-		echo "source $full_path" >> ~/.bashrc
-	fi
+    if ! grep "source $full_path" ~/.bashrc > /dev/null; then
+        echo "Adding $f source file to the bashrc."
+        echo "source $full_path" >> ~/.bashrc
+    fi
 done
