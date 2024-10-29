16bb0a12a4713f98ad15357fbacf26faae1a436f
Andrian Moroi <andrian.moroi@qiagen.com>
2024-10-29 16:07:08 +0200

    Improve nvim status line.

--- nvim_config/lua/config/keybindings.lua
+++ nvim_config/lua/config/keybindings.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write file" })
-vim.keymap.set("n", "<leader>q", "<cmd>bdelete<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>q", ":bp|bd #<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>")
 
--- nvim_config/lua/plugins/echasnovski-mini.lua
+++ nvim_config/lua/plugins/echasnovski-mini.lua
@@ -9,17 +9,31 @@ return {
             content = {
                 active = function()
                     local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 1000000 })
+                    local fileStatus    = vim.bo.modified and "*" or ""
                     local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 1000000 })
                     local location      = MiniStatusline.section_location({ trunc_width = 75 })
                     local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
+                    local size          = vim.fn.getfsize(vim.fn.getreg('%'))
+                    local sizeFormat    = ""
+
+                    if size < 0 then
+                        sizeFormat = ""
+                    elseif size < 1024 then
+                        sizeFormat = string.format('(%dB)', size)
+                    elseif size < 1048576 then
+                        sizeFormat = string.format('(%.2f KiB)', size / 1024)
+                    else
+                        sizeFormat = string.format('(%.2f MiB)', size / 1048576)
+                    end
+
                     return MiniStatusline.combine_groups({
                         { hl = mode_hl,                  strings = { mode } },
                         '%<', -- Mark general truncate point
-                        { hl = 'MiniStatuslineFilename', strings = { "%{expand('%:~:.')}" } },
+                        { hl = 'MiniStatuslineFilename', strings = { "%{expand('%:~:.')}", fileStatus } },
                         '%=', -- End left alignment
                         { hl = 'MiniStatuslineDevinfo',  strings = { vim.fn.reg_recording() ~= "" and "Recording: " .. vim.fn.reg_recording() or "" } },
-                        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo, sizeFormat } },
                         '%<', -- Mark general truncate point
                         { hl = mode_hl, strings = { search, location } },
                     })
