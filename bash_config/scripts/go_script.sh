0a4837013a65f0c03865eb158028d82873b76263
Andrian Moroi <andrian.moroi@qiagen.com>
2024-11-04 09:29:18 +0200

    Use the old version of the g command and keep the new fzf version by pressing C-g.

--- bash_config/scripts/go_script.sh
+++ bash_config/scripts/go_script.sh
@@ -1,9 +1,22 @@
-g() {
-    local options=$(cat ~/.config/bookmarks.txt | awk 'NF { print $1;}')
+function _g_path() {
+    echo $(grep "^$1\s" $BOOKMARKS_FILE | awk -F ' ' '{print $2}')
+}
 
-    local selected=$(printf "%s\n" "${options[@]}" | fzf)
+function _g_options() {
 
-    local path=$(cat ~/.config/bookmarks.txt | grep "^\s*$selected" | awk '{ print $2; }')
+    if [ -f "$BOOKMARKS_FILE" ] && [ "${#COMP_WORDS[@]}" == "2" ]; then
+        local all_options=$(awk -F ' ' '{print $1}' $BOOKMARKS_FILE)
+        local show_options=$(compgen -W "$all_options" -- "${COMP_WORDS[1]}")
+
+        COMPREPLY=($show_options)
+    else
+        return
+    fi
+}
+
+# Go function - cd to a folder stored in bookmark
+function g() {
+    local path=`_g_path $1`
 
     cd $path
 }
@@ -16,6 +29,18 @@ e() {
     cd - >> /dev/null
 }
 
+complete -F _g_options g
+complete -F _g_options e
 
-bind -x '"\C-g": g'  # Binds Ctrl+g to `my_function`
+_goto_fzf() {
+    local options=$(cat ~/.config/bookmarks.txt | awk 'NF { print $1;}')
+
+    local selected=$(printf "%s\n" "${options[@]}" | fzf)
+
+    local path=$(cat ~/.config/bookmarks.txt | grep "^\s*$selected" | awk '{ print $2; }')
+
+    cd $path
+}
+
+bind -x '"\C-g": _goto_fzf'  # Binds Ctrl+g to `my_function`
 
