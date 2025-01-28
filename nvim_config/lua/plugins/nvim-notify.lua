adc98e4793a7ac1e6affc1aed066be083d137100
Andrian Moroi <andrian.moroi@qiagen.com>
2025-01-28 17:40:07 +0200

    fix: update notification positions

--- nvim_config/lua/plugins/nvim-notify.lua
+++ nvim_config/lua/plugins/nvim-notify.lua
@@ -1,8 +1,17 @@
 return {
     'rcarriga/nvim-notify',
     config = function()
-        vim.notify = function(user_config, inherit, global_config)
+        require("notify").setup({
+            merge_duplicates = false,
+            render = "minimal",
+            top_down = false,
+            timeout = 2000
+        })
 
+
+
+
+        vim.notify = function(user_config, inherit, global_config)
             if global_config and global_config.replace == 'vim-dadbod-ui-info' then
                 -- This is a workaround for the vim-dadbod-ui-info plugin.
                 global_config.replace = nil
