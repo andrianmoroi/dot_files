95ccab4d9508ad881eb91306e9da9dddcb2dd55b
Andrian Moroi <andrian.moroi@gmail.com>
2025-08-10 14:15:31 +0300

    [main]: feat!: refactor the neovim config into a single file.

--- windows_config/nvim_config/init.lua
+++ windows_config/nvim_config/init.lua
@@ -1,38 +1,298 @@
--- IMPORTANT: Settings should be loaded before lazy plugin manager, needs to be
--- setup the mapleader and maplocalleader
-require("config.settings")
+--------------------------------------------------------------------------------
+--- Leader key
+--------------------------------------------------------------------------------
 
-require("config.highlight_yank_text")
-require("config.lazy_setup")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-local options = {
-    ui = {
-        icons = vim.g.have_nerd_font and {} or {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            require = "🌙",
-            source = "📄",
-            start = "🚀",
-            task = "📌",
-            lazy = "💤 ",
+--------------------------------------------------------------------------------
+--- Load lazy.nvim
+--------------------------------------------------------------------------------
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+--------------------------------------------------------------------------------
+--- Options
+--------------------------------------------------------------------------------
+
+vim.o.winborder = "rounded"
+
+vim.opt.shell = "powershell"
+vim.opt.cmdheight = 0
+
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a" -- enables mouse support for all modes
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.cmd("set expandtab") -- use spaces for identation instead of tabs.
+
+-- show an additional message when in Insert/Visual/Replace mode
+-- no effect when cmdheight is 0
+vim.opt.showmode = false
+
+vim.opt.breakindent = true -- Keep same indenting for a new line
+vim.opt.undofile = true    -- save undo history in a file
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
+
+-- waiting milliseconds(after last typed characters) to write the swap file to disk.
+vim.opt.updatetime = 250
+
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 3000
+vim.opt.ttimeoutlen = 0
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = false
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 18
+
+vim.opt.colorcolumn = "80"
+
+--------------------------------------------------------------------------------
+--- Setup lazy
+--------------------------------------------------------------------------------
+require("lazy").setup({
+
+    {
+        "rebelot/kanagawa.nvim",
+        opts = {
+            compile = false,             -- enable compiling the colorscheme
+            undercurl = true,            -- enable undercurls
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true},
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = false,         -- do not set background color
+            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+            colors = {                   -- add/modify theme and palette colors
+                palette = {},
+                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+            },
+            overrides = function(colors) -- add/modify highlights
+                return {}
+            end,
+            theme = "wave",              -- Load "wave" theme
+            background = {               -- map the value of 'background' option to a theme
+                dark = "wave",           -- try "dragon" !
+                light = "lotus"
+            },
+        },
+        config = function(_, opts)
+
+            require("kanagawa").setup(opts)
+
+            vim.cmd("colorscheme kanagawa")
+
+            vim.cmd [[highlight Visual guibg=#525249]]
+            vim.cmd [[highlight CurSearch cterm=bold gui=bold guifg=#dcd7ba guibg=#525249]]
+        end
+    },
+
+    { "nvim-tree/nvim-web-devicons", opts = {} },
+    {
+        "echasnovski/mini.files",
+        version = '*',
+        opts = {
+            mappings = {
+                -- close = '<C-e>'
+                go_in_plus = "<Enter>"
+            },
+            windows = {
+                preview = true,
+                width_preview = 50,
+            },
+        }
+    },
+    { "echasnovski/mini.pick", opts = {} },
+    {
+        "echasnovski/mini.surround",
+        opts = {
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                add = 'sa', -- Add surrounding in Normal and Visual modes
+                delete = 'sd', -- Delete surrounding
+                find = 'sf', -- Find surrounding (to the right)
+                find_left = 'sF', -- Find surrounding (to the left)
+                highlight = 'sh', -- Highlight surrounding
+                replace = 'sr', -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+
+                suffix_last = 'l', -- Suffix to search with "prev" method
+                suffix_next = 'n', -- Suffix to search with "next" method
+            },
+        }
+    },
+    {
+        "echasnovski/mini.statusline",
+        opts = {
+            use_icons = vim.g.have_nerd_font,
+            set_vim_settings = true,
+            content = {
+                active = function()
+                    local fileformat_icon  = function()
+                        local icons = { unix = ' LF', dos = ' CRLF', mac = ' CR' }
+                        return icons[vim.bo.fileformat] or vim.bo.fileformat
+                    end
+
+                    local mode, mode_hl    = MiniStatusline.section_mode({ trunc_width = 1000000 })
+                    local fileStatus       = vim.bo.modified and "*" or ""
+                    local fileinfo         = MiniStatusline.section_fileinfo({ trunc_width = 1000000 })
+                    -- local location      = '%P %l[%L]:%2v[%-2{virtcol("$") - 1}]'
+                    local location         = '%P of %L'
+                    local search           = MiniStatusline.section_searchcount({ trunc_width = 10 })
+                    local lsp              = MiniStatusline.section_lsp({ trunc_width = 75 })
+                    local diagnostics      = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+
+                    local size             = vim.fn.getfsize(vim.fn.getreg('%'))
+                    local sizeFormat       = ""
+                    local spell            = vim.api.nvim_get_option_value("spell", {}) and " " or ""
+
+                    if size < 0 then
+                        sizeFormat = ""
+                    elseif size < 1024 then
+                        sizeFormat = string.format('%d B', size)
+                    elseif size < 1048576 then
+                        sizeFormat = string.format('%.2f KiB', size / 1024)
+                    else
+                        sizeFormat = string.format('%.2f MiB', size / 1048576)
+                    end
+
+                    return MiniStatusline.combine_groups({
+                        { hl = mode_hl,                  strings = { mode } },
+                        '%<', -- Mark general truncate point
+                        { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics, spell, lsp } },
+                        { hl = 'MiniStatuslineFilename', strings = { "%{expand('%:~:.')}", fileStatus } },
+                        '%=', -- End left alignment
+                        { hl = 'MiniStatuslineDevinfo',  strings = { vim.fn.reg_recording() ~= "" and "Recording: " .. vim.fn.reg_recording() or "" } },
+                        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo, sizeFormat, fileformat_icon() } },
+                        '%<', -- Mark general truncate point
+                        { hl = mode_hl, strings = { search, location } },
+                    })
+                end,
+                inactive = function()
+                    return "%{expand('%:~:.')}"
+                end
+            }
+        }
+    },
+
+    {
+        "j-hui/fidget.nvim",
+        opts = {
+            notification = {
+                window = {
+                    winblend = 0
+                }
+            }
+        },
+        config = function(_, opts)
+            local fidget = require("fidget")
+
+            fidget.setup(opts)
+
+            vim.notify = fidget.notify
+        end
+    },
+
+    { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "helix",
         },
     },
-}
+    { "folke/zen-mode.nvim", opts = {} },
 
-require("lazy").setup({
-    spec = {
-        import = "plugins",
-    },
+    -- gitsign
+    --
+    -- lsp
+    -- treessiter
+    -- format
 
-    checker = { enabled = true },
-}, options)
+})
 
-require("config.keybindings")
-require("config.custom_modes")
+
+--------------------------------------------------------------------------------
+--- Keymaps
+--------------------------------------------------------------------------------
+
+local map = function(mode, key, action, description)
+    vim.keymap.set(mode, key, action, { desc = description})
+end
+
+map('n', "<leader>w", ":w!<CR>", "Save file.")
+map('n', "<M-q>", ":q<CR>", "Close window.")
+map('n', "<Esc>", ":nohlsearch<CR>", "Disable highlight search.")
+map('n', "<Tab>", ":bnext<CR>", "Switch to next buffer.")
+map('n', "<S-Tab>", ":bprevious<CR>", "Switch to previous buffer.")
+map('n', "<leader>q", ":bdelete<CR>", "Close buffer.")
+map('n', "<C-n>", ":enew<CR>", "New empty buffer.")
+map('n', "<C-w>n", ":vnew<CR>", "New vertical empty buffer.")
+map('n', "<C-j>", ":cnext<CR>", "Next quickfix bookmark.")
+map('n', "<C-k>", ":cprevious<CR>", "Previous quickfix bookmark.")
+
+map('i', "<S-Tab>", "<C-V><Tab>", "Insert tab character.")
+
+map('t', "<Esc><Esc>", "<C-\\><C-n>", "Exit terminal mode.")
+
+map({'n', 'x'}, "<leader>y", "\"+y", "Copy to clipboard.")
+map({'n', 'x'}, "<leader>p", "\"+p", "Paste from clipboard.")
+map({'n', 'x'}, "<leader>P", "\"+P", "Paste from clipboard.")
+
+map('n', "<C-e>", function() MiniFiles.open() end, "Open file explorer.")
+map('n', "<C-p>", function() MiniPick.builtin.files() end, "Search by file names.")
+map('n', "<leader>sg", function() MiniPick.builtin.grep_live() end, "Search in files.")
+map('n', "<leader>sh", function() MiniPick.builtin.help() end, "Search healp.")
+
+--------------------------------------------------------------------------------
+--- Highlight yanked text
+--------------------------------------------------------------------------------
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
