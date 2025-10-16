aea13104ba10b0e5515aad5854bd107eb33c9b34
Andrian Moroi <andrian.moroi@gmail.com>
2025-10-16 10:26:27 +0300

    [main]: Improve nvim terminal setup for powershell

--- windows_config/nvim_config/init.lua
+++ windows_config/nvim_config/init.lua
@@ -23,7 +23,36 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
+
+--------------------------------------------------------------------------------
+--- Shell option
+--------------------------------------------------------------------------------
 vim.opt.rtp:prepend(lazypath)
+--
+-- Use pwsh if available, otherwise fallback to powershell
+vim.o.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
+
+-- Set shell command flags
+vim.o.shellcmdflag = table.concat({
+  '-NoLogo',
+  '-NonInteractive',
+  '-ExecutionPolicy RemoteSigned',
+  '-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();',
+  "$PSDefaultParameterValues['Out-File:Encoding']='utf8';",
+  "$PSStyle.OutputRendering='plaintext';",
+  'Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+}, ' ')
+
+-- Set shell redirection
+vim.o.shellredir  = '2>&1 | %{{ "$_" }} | Out-File %s; exit $LastExitCode'
+vim.o.shellpipe   = '2>&1 | %{{ "$_" }} | tee %s; exit $LastExitCode'
+
+-- Disable shell quoting
+vim.o.shellquote  = ''
+vim.o.shellxquote = ''
+
+-- vim.env.TMP = vim.fn.expand("~/nvim-temp/")
+-- vim.env.TEMP = vim.fn.expand("~/nvim-temp/")
 
 
 --------------------------------------------------------------------------------
@@ -32,7 +61,6 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.o.winborder = "rounded"
 
-vim.opt.shell = "powershell"
 vim.opt.shellcmdflag = "-c"
 vim.opt.cmdheight = 0
 
