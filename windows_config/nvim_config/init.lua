88cb9a4fd52a58ac5badca207c7f1454e176d130
Andrian Moroi <andrian.moroi@gmail.com>
2026-04-06 13:57:03 +0300

    [main]: Enable UI2.0 for nvim.

--- windows_config/nvim_config/init.lua
+++ windows_config/nvim_config/init.lua
@@ -5,7 +5,44 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- vim.keymap.set("i", "jk", "<Esc>", { desc = "Exist insert mode." })
+
+-------------------------------------------------------
+--- UI 2.0
+-------------------------------------------------------
+
+require('vim._core.ui2').enable({
+    enable = true,   -- Whether to enable or disable the UI.
+    msg = {          -- Options related to the message module.
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = 'cmd',
+        cmd = {           -- Options related to messages in the cmdline window.
+            height = 0.5  -- Maximum height while expanded for messages beyond 'cmdheight'.
+        },
+        dialog = {        -- Options related to dialog window.
+            height = 0.5, -- Maximum height.
+        },
+        msg = {           -- Options related to msg window.
+            height = 0.5, -- Maximum height.
+            timeout = 4000, -- Time a message is visible in the message window.
+        },
+        pager = {         -- Options related to message window.
+            height = 1,   -- Maximum height.
+        },
+    },
+})
+
+-------------------------------------------------------
+--- Load vim.pack
+-------------------------------------------------------
+
+-- vim.pack.add({
+--     -- "https://github.com/tpope/vim-fugitive", 
+--     --    "https://github.com/lewis6991/gitsigns.nvim",
+-- }, { load = true })
+
 
 -------------------------------------------------------
 --- Load lazy.nvim
@@ -512,7 +549,7 @@ vim.diagnostic.config({
 --- Treesitter
 -------------------------------------------------------
 
-vim.opt.rtp:prepend(vim.fn.stdpath('data').."/site")
+vim.opt.rtp:prepend(vim.fn.stdpath('data') .. "/site")
 
 -------------------------------------------------------
 --- Loremipsum
