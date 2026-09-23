97915553523f1c66171c8db9edd1f7c79f2e8a21
Andrian Moroi <andrian.moroi@gmail.com>
2026-09-23 11:14:02 +0300

    Add keymap for restart roslyn lsp.

--- windows_config/nvim_config/init.lua
+++ windows_config/nvim_config/init.lua
@@ -267,6 +267,7 @@ map('n', "<leader>gp", require("git").toggle_preview_hunk, "Git toggle preview h
 map('n', "<leader>ms", require("myhttp").send, "Send HTTP Request.")
 map('v', "<leader>cj", ":!jq<CR>", "Format JSON.")
 map('n', "<leader>cj", ":.!jq<CR>", "Format JSON.")
+map('n', "<leader>cr", ":Roslyn restart<CR>", "Restart roslyn server.")
 
 ---@type Gitsigns.NavOpts
 local gitsign_hunk_config = {
